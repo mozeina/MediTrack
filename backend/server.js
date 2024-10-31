8f790670e35c6f99738cd9cf77dade73468974ba
mohammed <zeinaboy@gmail.com>
2024-10-31 13:24:17 +0000

    update frontend url for cors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'https://meditrack1.onrender.com' }));
 
 
 const port = process.env.PORT || 7777; 
